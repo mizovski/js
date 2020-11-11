332f605b61317e1681ff4983224895399fe5009a
mizovski <72278088+mizovski@users.noreply.github.com>
2020-11-11 19:42:55 +0100

    js1
    cas1

---
+++ index.js
@@ -0,0 +1,36 @@
+/*deklariranje promeni */
+
+let name = 'Nikola';
+let year = 2020;
+
+
+//pecatenje na promenlva vo konzola
+console.log(name);
+console.log(year)
+
+/* String ''
+Number 12
+boolean true , false
+undefined
+null
+*/
+//nekoi primitivni tipovi
+
+
+let person = {name: 'Nikola' , year: 2020 };
+//let people = [{name: 'Nikola' , year: 2020 } , {name: 'Frosina' year: 2020 }];
+let isHot  = false;
+let weather = undefined;
+let car = null;
+
+console.log('isHot type has ' + typeof(isHot));
+
+const pi = 3.14;
+console.log(pi);
+
+let numberOne = 1 ;
+let numberTwo = 2 ;
+
+console.log(numberOne + numberTwo);
+
+console.log(numberOne != numberTwo);
